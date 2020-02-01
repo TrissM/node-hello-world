bc3e465637c0cda0d667ec0f73145fc13a19dd52
Tristan Moss <mr.tristan.moss@gmail.com>
2020-02-01 23:54:33 +0000

    Update app.js
    Changed content type to HTML

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const http = require('http');
 
 http.createServer(function(request, response) {
-  response.writeHead(200, {'Content-Type': 'text/plain'});
-  response.end("Hello, Tristan!\n");
+  response.writeHead(200, {'Content-Type': 'text/html'});
+  response.end("<h1>Hello, Tristan!</h1>");
 }).listen(process.env.PORT);
 
 console.log('App is running...');
